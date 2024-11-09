25f5b779045d92559f486df9e1d4c613d29fca05
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2024-11-09 19:58:57 +0200

    render.com "Port" trouble shooting

--- main.ts
+++ main.ts
@@ -1,6 +1,25 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import bot from './src/bot';
+import http from 'http';
 
-bot.launch();
-console.log('Bot is running...');
+const port = process.env.PORT || 3000;
+
+
+bot.launch()
+    .then(() => {
+        console.log("Bot is running with long polling...");
+    })
+    .catch((err) => {
+        console.error("Failed to launch bot:", err);
+    });
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Bot is running\n');
+});
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
