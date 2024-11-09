b36a1d575c19a757167ae9c5604f843fad400340
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2024-11-09 22:47:41 +0200

    keepAlive server on render

--- main.ts
+++ main.ts
@@ -6,20 +6,35 @@ import http from 'http';
 const port = process.env.PORT || 3000;
 
 
-bot.launch()
-    .then(() => {
-        console.log("Bot is running with long polling...");
-    })
-    .catch((err) => {
-        console.error("Failed to launch bot:", err);
-    });
+const keepAlive = () => {
+  setInterval(() => {
+    const url = process.env.SERVER_URL || 'http://localhost:' + port;
+    http
+      .get(url, (res) => {
+        res.on('data', () => {});
+        res.on('end', () => {});
+      })
+      .on('error', (err) => {
+        console.error('Error in keepAlive:', err.message);
+      });
+  }, 45000); 
+};
 
+(async () => {
+  try {
+    await bot.launch();
+    console.log('Bot is running with long polling...');
+  } catch (err) {
+    console.error('Failed to launch bot:', err);
+  }
 
-const server = http.createServer((req, res) => {
+  const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Bot is running\n');
-});
+  });
 
-server.listen(port, () => {
+  server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
-});
+    keepAlive(); 
+  });
+})();
