229242d6a1a7086677ea33508f5bff5f2751e74c
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2024-11-10 00:41:23 +0200

    helthcheck

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import bot from './src/bot';
 import http from 'http';
+import https from 'https';
 
 const port = process.env.PORT || 3000;
 
@@ -14,23 +15,33 @@ bot.launch()
   });
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Bot is running\n');
+  if (req.url === '/healthcheck') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Healthcheck pass\n');
+    console.log('Healthcheck pass');
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Bot is running\n');
+  }
 });
 
 server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
-  keepAlive(); 
+  keepAlive();
 });
 
 const keepAlive = () => {
   setInterval(() => {
     const url = process.env.SERVER_URL || 'http://localhost:' + port;
-    http.get(url, (res) => {
+    const client = url.startsWith('https') ? https : http;
+    
+    client.get(url + '/healthcheck', (res) => {
       res.on('data', () => {});
-      res.on('end', () => {});
+      res.on('end', () => {
+        console.log('Healthcheck passed');
+      });
     }).on('error', (err) => {
       console.error('Error in keepAlive:', err.message);
     });
-  }, 45000); 
+  }, 45000);
 };
