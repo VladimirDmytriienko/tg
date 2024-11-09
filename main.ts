dfc80ab676aaf4baa28610445a971b7b7e4b96e1
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2024-11-09 23:44:51 +0200

    maintence fix

--- main.ts
+++ main.ts
@@ -2,39 +2,36 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import bot from './src/bot';
 import http from 'http';
+import https from 'https';
 
 const port = process.env.PORT || 3000;
 
+bot.launch()
+  .then(() => {
+    console.log('Bot is running with long polling...');
+  })
+  .catch((err) => {
+    console.error('Failed to launch bot:', err);
+  });
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot is running\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+  keepAlive(); 
+});
 
 const keepAlive = () => {
   setInterval(() => {
     const url = process.env.SERVER_URL || 'http://localhost:' + port;
-    http
-      .get(url, (res) => {
-        res.on('data', () => {});
-        res.on('end', () => {});
-      })
-      .on('error', (err) => {
-        console.error('Error in keepAlive:', err.message);
-      });
+    http.get(url, (res) => {
+      res.on('data', () => {});
+      res.on('end', () => {});
+    }).on('error', (err) => {
+      console.error('Error in keepAlive:', err.message);
+    });
   }, 45000); 
 };
-
-(async () => {
-  try {
-    await bot.launch();
-    console.log('Bot is running with long polling...');
-  } catch (err) {
-    console.error('Failed to launch bot:', err);
-  }
-
-  const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Bot is running\n');
-  });
-
-  server.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-    keepAlive(); 
-  });
-})();
